22400869cf4a0ff977639ffae6959faca01fe13d
jonnybarnes <jonnybarnes@gmail.com>
2013-06-14 15:14:22 +0100

    Split smartQuotes into submethods and added comments

--- src/Jonnybarnes/Dumbquotes/DumbquotesParser.php
+++ src/Jonnybarnes/Dumbquotes/DumbquotesParser.php
@@ -2,14 +2,61 @@
 
 class DumbquotesParser {
 	/*
-	 * we'll do a striahgt swap for now
+	 * We swap all the dumb quotes for smart quotes
 	 * 
 	 * @returns string
 	 */
 	public function smartQuotes($text)
 	{
-		$smartquotes = str_replace('\'', '‘', $text);
+		$apostrophe = $this->apostrophe($text);
 
-		return $smartquotes;
+		$singlequotes = $this->singleQuotes($apostrophe);
+
+		$doublequotes = $this->doubleQuotes($singlequotes);
+
+		return $doublequotes;
+	}
+
+	/*
+	 * This looks for single apostrophes
+	 *
+	 * @returns string
+	 */
+	public function apostrophe($text)
+	{
+		$apostrophes = str_replace('\'s', '’s', $text);
+		$sapostrophe = str_replace('s\' ', 's’ ', $apostrophes);
+
+		return $sapostrophe;
+	}
+
+	/*
+	 * The looks for single quotes, i.e. 'Some text'
+	 *
+	 * @returns string
+	 */
+	public function singleQuotes($text)
+	{
+		$singlequotesPattern = "/'\b(.*)\b'/";
+		$singlequotesReplacement = "‘$1’";
+
+		$singlequotes = preg_replace($singlequotesPattern, $singlequotesReplacement, $text);
+
+		return $singlequotes;
+	}
+
+	/*
+	 * This looks for double quotes, i.e. "Some text"
+	 *
+	 * @returns string
+	 */
+	public function doubleQuotes($text)
+	{
+		$doublequotesPattern = '/"\b(.*)\b"/';
+		$doublequotesReplacment = '“$1”';
+
+		$doublequotes = preg_replace($doublequotesPattern, $doublequotesReplacment, $text);
+
+		return $doublequotes;
 	}
 }
