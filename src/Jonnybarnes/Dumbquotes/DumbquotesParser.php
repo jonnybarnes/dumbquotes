8823d2b8abe9a79bcb39b3db90034e042027a897
jonnybarnes <jonnybarnes@gmail.com>
2013-06-16 19:14:37 +0100

    Added code to not transform text inside code tag

--- src/Jonnybarnes/Dumbquotes/DumbquotesParser.php
+++ src/Jonnybarnes/Dumbquotes/DumbquotesParser.php
@@ -1,6 +1,23 @@
 <?php namespace Jonnybarnes\Dumbquotes;
 
 class DumbquotesParser {
+	/**
+	 * This tranforms the text unless its in a code block
+	 * taken from http://stackoverflow.com/a/1472866/12854
+	 *
+	 * @returns string
+	 */
+	public function transform($content)
+	{
+		$pattern = "#\\<code>.*?\\</code>#i";
+
+		if(preg_match_all($pattern, $content, $codeBlocks)) {
+			return $this->arrayJoin($codeBlocks[0], array_map('self::smartQuotes', preg_split($pattern, $content)));
+		}
+
+		return $this->smartQuotes($content);
+	}
+
 	/*
 	 * We swap all the dumb quotes for smart quotes
 	 * 
@@ -41,7 +58,7 @@ class DumbquotesParser {
 	 */
 	public function singleQuotes($text)
 	{
-		$singlequotesPattern = "/'(.*)'/";
+		$singlequotesPattern = "/'(.*?)'/";
 		$singlequotesReplacement = "‘$1’";
 
 		$singlequotes = preg_replace($singlequotesPattern, $singlequotesReplacement, $text);
@@ -56,11 +73,38 @@ class DumbquotesParser {
 	 */
 	public function doubleQuotes($text)
 	{
-		$doublequotesPattern = '/"(.*)"/';
+		$doublequotesPattern = '/"(.*?)"/';
 		$doublequotesReplacment = '“$1”';
 
 		$doublequotes = preg_replace($doublequotesPattern, $doublequotesReplacment, $text);
 
 		return $doublequotes;
+	}
+
+	/*
+	 * This function pieces together transformed text with untransformed code text
+	 * taken from http://stackoverflow.com/a/1472866/12854
+	 *
+	 * @returns string
+	 */
+	public function arrayJoin(array $glue, array $pieces)
+	{
+		$glue = array_values($glue);
+		$pieces = array_values($pieces);
+		$piecesSize = count($pieces);
+
+		if(count($glue) + 1 != $piecesSize) {
+			return false;
+		}
+
+		$joined = array();
+		for($i = 0; $i < $piecesSize; $i++) {
+			$joined[] = $pieces[$i];
+			if(isset($glue[$i])) {
+				$joined[] = $glue[$i];
+			}
+		}
+
+		return implode('', $joined);
 	}
 }
